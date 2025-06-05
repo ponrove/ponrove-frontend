324fc5016f21510a118eb1f870cf70caa465c55b
Simon Wikstrand <simon@wikstrand.dev>
2025-06-05 16:04:52 +0200

    added dockerfile to build the project

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 		  fallback: "index.html", // for a pure single-page application mode
-			pages: '../pkg/app/build'
+			// pages: '../pkg/app/build'
 		})
 	}
 };
